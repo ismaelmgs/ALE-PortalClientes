1cb2e30e9a69ba61bf26c910f0ffbb07a7f8794c
ismaelmgs <maick.mor@gmail.com>
2021-08-23 20:32:23 -0500

    Version inicial
    Se sube por primera vez el codigo

---
+++ PortalClientes/Presenter/Dashboard_Presenter.cs
@@ -0,0 +1,19 @@
+﻿using PortalClientes.DomainModel;
+using PortalClientes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalClientes.Presenter
+{
+    public class Dashboard_Presenter : BasePresenter<IViewDashboard>
+    {
+        private readonly DBDashboard oIGesCat;
+
+        public Dashboard_Presenter(IViewDashboard oView, DBDashboard oGC) : base(oView)
+        {
+            oIGesCat = oGC;
+        }
+    }
+}
---
+++ PortalClientes/Views/frmDashboard.aspx.cs
@@ -0,0 +1,37 @@
+﻿using PortalClientes.DomainModel;
+using PortalClientes.Interfaces;
+using PortalClientes.Presenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PortalClientes.Views
+{
+    public partial class frmDashboard : System.Web.UI.Page, IViewDashboard
+    {
+        #region EVENTOS
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            oPresenter = new Dashboard_Presenter(this, new DBDashboard());
+        }
+        #endregion
+
+
+        #region METODOS
+        #endregion
+
+
+        #region VARIABLES Y PROPIEDADES
+        Dashboard_Presenter oPresenter;
+        public event EventHandler eNewObj;
+        public event EventHandler eObjSelected;
+        public event EventHandler eSaveObj;
+        public event EventHandler eDeleteObj;
+        public event EventHandler eSearchObj;
+
+        #endregion
+    }
+}
