c74525e7c2ecfdcce15577bcf30537f76449c383
ismaelmgs <maick.mor@gmail.com>
2021-08-25 00:54:56 -0500

    Usuarios

---
+++ PortalClientes/Clases/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalClientes.Clases
+{
+    public class Helper
+    {
+        public const string Usuarios = "U1";
+        public const string Dashboard = "D1";
+        public const string Aeronaves = "A1";
+        public const string Calendario = "C1";
+        public const string EstadoCuenta = "E1";
+        public const string Tripulacion = "T1";
+        public const string Metricas = "M1";
+    }
+}
---
+++ PortalClientes/Views/frmUsuarios.aspx.cs
@@ -0,0 +1,39 @@
+﻿using PortalClientes.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NucleoBase.Core;
+
+namespace PortalClientes.Views
+{
+    public partial class frmUsuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LlenaGrid();
+        }
+
+
+        private void LlenaGrid()
+        {
+            List<Usuario> ListUsers = new List<Usuario>();
+            for (int i = 0; i < 5; i++)
+            {
+                Usuario oUser = new Usuario();
+                oUser.Correo = "Correo" + i.S();
+                oUser.Nombres = "Nombres " + i.S();
+                oUser.ApePat = "Paterno " + i.S();
+                oUser.ApeMat = "Materno " + i.S();
+                oUser.Puesto = "Puesto " + i.S();
+
+                ListUsers.Add(oUser);
+            }
+
+            gvUsuarios.DataSource = ListUsers;
+            gvUsuarios.DataBind();
+        }
+    }
+}
