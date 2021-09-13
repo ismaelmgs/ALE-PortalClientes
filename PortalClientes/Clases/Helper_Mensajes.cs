a418f458eda78df34ce9a56010171118f5b9c1ac
ismaelmgs <maick.mor@gmail.com>
2021-09-13 09:34:13 -0500

    Clase de mensajes

---
+++ PortalClientes/Clases/Helper_Mensajes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalClientes.Clases
+{
+    public class Helper_Mensajes
+    {
+        public string DevuelveMensajeError(string sCodigoError)
+        {
+            string sIdioma = Utils.Idioma;
+            string sMensaje = string.Empty;
+            switch (sCodigoError)
+            {
+                case "9999":
+                    switch (sIdioma)
+                    {
+                        case "es-MX":
+                            sMensaje = "El usuario y contraseña no existen.";
+                            break;
+                        case "en-US":
+                            sMensaje = "";
+                            break;
+                    }
+                    break;
+            }
+
+            return sMensaje;
+        }
+    }
+}
