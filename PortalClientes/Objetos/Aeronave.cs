2e11b875927bda6e8f0744ef2f3236172c311829
Fernando Gomez <dgomezpayan@gmail.com>
2021-10-06 16:14:45 -0600

    cambios en clases aeronave y responsegraficaGastos

--- PortalClientes/Objetos/Aeronave.cs
+++ PortalClientes/Objetos/Aeronave.cs
@@ -14,18 +14,18 @@ namespace PortalClientes.Objetos
         public string Modelo { get; set; }
         public string noRegistro { get; set; }
         public string noSerie { get; set; }
-        public int noPasajeros { get; set; }
-        public int noTripulacion { get; set; }
+        public int? noPasajeros { get; set; }
+        public int? noTripulacion { get; set; }
         public string dimencionesExteriores { get; set; }
         public string dimencionesInteriores { get; set; }
-        public int maxGasolina { get; set; }
-        public int minGasolina { get; set; }
-        public int velocidadCrucero { get; set; }
-        public int altitudMaxima { get; set; }
+        public int? maxGasolina { get; set; }
+        public int? minGasolina { get; set; }
+        public int? velocidadCrucero { get; set; }
+        public int? altitudMaxima { get; set; }
         public string tipoGasolina { get; set; }
-        public int Rendimiento { get; set; }
-        public int Distancia { get; set; }
-        public int Peso { get; set; }
+        public int? Rendimiento { get; set; }
+        public int? Distancia { get; set; }
+        public int? Peso { get; set; }
         public IEnumerable<FotoAeronave> Imagenes { get; set; }
 
     }
--- PortalClientes/Objetos/responseGraficaGastos.cs
+++ PortalClientes/Objetos/responseGraficaGastos.cs
@@ -8,8 +8,8 @@ namespace PortalClientes.Objetos
     public class responseGraficaGastos
     {
         public int idRubro { get; set; }
-        public string rubroEsp { get; set; }
-        public string rubroEng { get; set; }
+        public string rubroESP { get; set; }
+        public string rubroENG { get; set; }
         public double totalMXN { get; set; }
         public double totalUSD { get; set; }
         public List<gasto> Gastos { get; set; }
@@ -19,14 +19,15 @@ namespace PortalClientes.Objetos
     public class gasto
     {
         public int idRubro { get; set; }
-        public string rubroEsp { get; set; }
-        public string rubroEng { get; set; }
+        public string rubroESP { get; set; }
+        public string rubroENG { get; set; }
         public double totalMXN { get; set; }
         public double totalUSD { get; set; }
-        public DateTime fecha { get; set; }
+        public string fecha { get; set; }
         public string categoria { get; set; }
         public string tipodeGasto { get; set; }
         public string comentarios { get; set; }
         public string mes { get; set; }
     }
+
 }
