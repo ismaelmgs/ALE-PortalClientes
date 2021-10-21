0a27325a45402e74ce6ae48b8556d36b34571a54
IvanMorato <77359536+IvanMorato@users.noreply.github.com>
2021-10-21 09:53:22 -0500

    Reporte DetalleGastos Metricas

---
+++ PortalClientes/DomainModel/DBMetricasEstatics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using System.Web.Script.Serialization;
+using PortalClientes.Clases;
+using PortalClientes.Objetos;
+
+namespace PortalClientes.DomainModel
+{
+    public class DBMetricasEstatics
+    {
+        public List<responseGraficaGastos> ObtenerGastos(FiltroGraficaGastos filtro)
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            ser.MaxJsonLength = 500000000;
+            List<responseGraficaGastos> d = new List<responseGraficaGastos>();
+            FiltroGraficaGastos oLog = new FiltroGraficaGastos();
+            oLog = filtro;
+            oLog.matricula = Utils.MatriculaActual;
+            oLog.idioma = Utils.Idioma;
+
+            TokenWS oToken = Utils.ObtieneToken;
+
+            var client = new RestClient(Helper.D_UrlObbtenerGastoRubros);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", oToken.token);
+            request.AddJsonBody(oLog);
+
+            IRestResponse response = client.Execute(request);
+            var resp = response.Content;
+
+            d = ser.Deserialize<List<responseGraficaGastos>>(resp);
+
+            return d;
+        }
+    }
+}
