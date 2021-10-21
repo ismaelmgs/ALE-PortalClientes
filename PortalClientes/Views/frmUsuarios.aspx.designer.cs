ae7c111362b701c086fccdce452a1c5de897ed0a
Fernando Gomez <dgomezpayan@gmail.com>
2021-10-21 17:08:53 -0600

    cambios en vista de modulo usuarios

--- PortalClientes/Views/frmUsuarios.aspx.designer.cs
+++ PortalClientes/Views/frmUsuarios.aspx.designer.cs
@@ -130,13 +130,22 @@ namespace PortalClientes.Views {
         protected global::System.Web.UI.WebControls.Label lblTituloModalUsuario;
         
         /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+        
+        /// <summary>
         /// Control txtNombre.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::DevExpress.Web.Bootstrap.BootstrapTextBox txtNombre;
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
         
         /// <summary>
         /// Control lblReqNombre.
