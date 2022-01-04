faa6a933b71d86e1786e77b9e9c78d3cfb703b07
Fernando Gomez <dgomezpayan@gmail.com>
2022-01-04 10:44:53 -0700

    Cambios en menu lateral

--- PortalClientes/Site1.Master.cs
+++ PortalClientes/Site1.Master.cs
@@ -68,8 +68,8 @@ namespace PortalClientes
 
         private void ArmaFormulario()
         {
-            MenuMatriculas.Items[0].Text = Properties.Resources.CerrarSesion;
-            MenuMatriculas.Items[1].Text = Utils.NombreUsuario;
+            MenuMatriculas.Items[3].Text = Properties.Resources.CerrarSesion;
+            MenuMatriculas.Items[0].Text = Utils.NombreUsuario;
             MenuMatriculas.Items[2].Text = Properties.Resources.AdministrarCuenta;
             
         }
@@ -84,15 +84,6 @@ namespace PortalClientes
                 string sNombre = Utils.Idioma == "es-MX" || Utils.Idioma == string.Empty ? oMenu.nombreESP : oMenu.nombreUSD;
                 sHtml += "<li><a href = '" + oMenu.urlPage + "'><i class='" + oMenu.style + "'></i>"+ sNombre + "</a></li>";
             }
-                                //<li><a href = "frmDashboard.aspx">< i class="fa fa-th-large"></i>Dashboard</a></li>
-                                //<li><a href = "frmTuAeronave.aspx">< i class="fa fa-plane"></i>Tu Aeronave</a></li>
-                                //<li><a href = "frmCalendario.aspx">< i class="fa fa-calendar"></i>Calendario</a></li>
-                                //<li><a href = "frmEstadoCuenta.aspx">< i class="fa fa-list"></i>Estados de Cuenta</a></li>
-                                //<li><a href = "frmTripulacion.aspx">< i class="fa fa-male"></i>Tripulación</a></li>
-                                //<li><a href = "frmMetricasEstadisticas.aspx">< i class="fa fa-line-chart"></i>Métricas y Estadísticas</a></li>
-                                //<li><a href = "frmReportes.aspx">< i class="fa fa-list-alt"></i>Reportes</a></li>
-                                //<li><a href = "frmMantenimientos.aspx">< i class="fa fa-wrench"></i>Mantenimientos</a></li>
-                                //<li><a href = "frmUsuarios.aspx">< i class="fa fa-group"></i>Usuarios</a></li>
 
             sHtml += " </ul>";
 
@@ -142,76 +133,6 @@ namespace PortalClientes
                 }
 
                 return olstFinal;
-                
-                /*
-                MenuDinamico oMenu1 = new MenuDinamico();
-                oMenu1.UrlPage = "frmDashboard.aspx";
-                oMenu1.Style = "fa fa-th-large";
-                oMenu1.NombreESP = "Dashboard";
-                oMenu1.NombreUSD = "Dashboard";
-
-                MenuDinamico oMenu2 = new MenuDinamico();
-                oMenu2.UrlPage = "frmTuAeronave.aspx";
-                oMenu2.Style = "fa fa-plane";
-                oMenu2.NombreESP = "Tu Aeronave";
-                oMenu2.NombreUSD = "Your Aircraft";
-
-                MenuDinamico oMenu3 = new MenuDinamico();
-                oMenu3.UrlPage = "frmCalendario.aspx";
-                oMenu3.Style = "fa fa-calendar";
-                oMenu3.NombreESP = "Calendario";
-                oMenu3.NombreUSD = "Calendar";
-
-                MenuDinamico oMenu4 = new MenuDinamico();
-                oMenu4.UrlPage = "frmEstadoCuenta.aspx";
-                oMenu4.Style = "fa fa-list";
-                oMenu4.NombreESP = "Estados de Cuenta";
-                oMenu4.NombreUSD = "Account statements";
-
-                MenuDinamico oMenu5 = new MenuDinamico();
-                oMenu5.UrlPage = "frmTripulacion.aspx";
-                oMenu5.Style = "fa fa-male";
-                oMenu5.NombreESP = "Tripulación";
-                oMenu5.NombreUSD = "Crew";
-
-                MenuDinamico oMenu6 = new MenuDinamico();
-                oMenu6.UrlPage = "frmMetricasEstadisticas.aspx";
-                oMenu6.Style = "fa fa-line-chart";
-                oMenu6.NombreESP = "Métricas y Estadísticas";
-                oMenu6.NombreUSD = "Metrics and Statistics";
-
-                MenuDinamico oMenu7 = new MenuDinamico();
-                oMenu7.UrlPage = "frmReportes.aspx";
-                oMenu7.Style = "fa fa-list-alt";
-                oMenu7.NombreESP = "Reportes";
-                oMenu7.NombreUSD = "Reports";
-
-                MenuDinamico oMenu8 = new MenuDinamico();
-                oMenu8.UrlPage = "frmMantenimientos.aspx";
-                oMenu8.Style = "fa fa-wrench";
-                oMenu8.NombreESP = "Mantenimientos";
-                oMenu8.NombreUSD = "Maintenance";
-
-                MenuDinamico oMenu9 = new MenuDinamico();
-                oMenu9.UrlPage = "frmUsuarios.aspx";
-                oMenu9.Style = "fa fa-group";
-                oMenu9.NombreESP = "Usuarios";
-                oMenu9.NombreUSD = "Users";
-
-
-                olst.Add(oMenu0);
-                olst.Add(oMenu1);
-                olst.Add(oMenu2);
-                olst.Add(oMenu3);
-                olst.Add(oMenu4);
-                olst.Add(oMenu5);
-                olst.Add(oMenu6);
-                olst.Add(oMenu7);
-                olst.Add(oMenu8);
-                olst.Add(oMenu9);
-
-                return olst;
-                */
             }
             catch (Exception ex)
             {
@@ -227,7 +148,7 @@ namespace PortalClientes
                 if (oU != null)
                 {
                     BootstrapMenuItem oMenuMats = new BootstrapMenuItem();
-                    oMenuMats = MenuMatriculas.Items[3];
+                    oMenuMats = MenuMatriculas.Items[1];
 
                     if (oU.lsMatriculas != null)
                     {
