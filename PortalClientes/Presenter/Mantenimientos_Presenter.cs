c86b8a760c7e9baba0fa2a3a9bdec0e64f818ff3
Fernando Gomez <dgomezpayan@gmail.com>
2022-02-10 15:57:31 -0700

    archivo faltante

---
+++ PortalClientes/Presenter/Mantenimientos_Presenter.cs
@@ -0,0 +1,26 @@
+﻿using PortalClientes.DomainModel;
+using PortalClientes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalClientes.Presenter
+{
+    public class Mantenimiemntos_Presenter : BasePresenter<iViewMantenimientos>
+    {
+        private readonly DBMantenimientos oIGesCat;
+
+        public Mantenimiemntos_Presenter(iViewMantenimientos _oIView, DBMantenimientos oGC) : base(_oIView)
+        {
+            oIGesCat = oGC;
+
+            oIView.eSearchObj += SearchObj_Presenter;
+        }
+
+        protected override void SearchObj_Presenter(object sender, EventArgs e)
+        {
+            oIView.cargarMantenimientos(oIGesCat.obtenerMttos(oIView.iMeses));
+        }
+    }
+}
