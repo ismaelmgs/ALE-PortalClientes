85d079fae9b720e6763261aa29e56b421200dcfb
Fernando Gomez <dgomezpayan@gmail.com>
2022-06-21 21:59:09 -0600

    cambios en idioma de pagina default y se arregla peticiones en tu aeronave

--- PortalClientes/Views/frmDefault.aspx.cs
+++ PortalClientes/Views/frmDefault.aspx.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using PortalClientes.Clases;
+
+
 
 namespace PortalClientes
 {
     public partial class frmDefault : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+            TextBox milabel = (TextBox)this.Master.FindControl("txtLang");
+            if (milabel.Text != Utils.Idioma && milabel.Text != string.Empty)
+            {
+                Utils.Idioma = milabel.Text;
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(Utils.Idioma);
+                ArmarDefault();
+            }
+            else
+            {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(Utils.Idioma);
+                ArmarDefault();
+            }
+           
+        }
+
+        private void ArmarDefault()
         {
             lblBienvenido.Text = Properties.Resources.Def_Bienvenidos;
             lblEstadoDeCuenta.Text = Properties.Resources.Def_NuestraFlota;
